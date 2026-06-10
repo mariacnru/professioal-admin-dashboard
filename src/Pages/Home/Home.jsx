0e91872eeeb7db1ab9f32798f9f55ebfb81ce905
maria <mariacnru@gmail.com>
2026-06-10 10:16:45 +0330

    Card modified , apexcharts added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import LastProducts from "./components/LastProducts";
 import LastUsers from "./components/LastUsers";
+import { NavLink } from "react-router";
 
 function Home() {
   const card = [
@@ -43,11 +44,24 @@ function Home() {
 
       <div className="lg:flex *:h-102 gap-5 w-full space-y-5 mt-5">
         <div className="bg-white p-5 rounded-md grow">
-          <h3 className="font-MorabbaMedium mb-5">آخرین محصولات اضافه شده</h3>
+          <div className="flex justify-between">
+            <h3 className="font-MorabbaMedium mb-5">آخرین محصولات اضافه شده</h3>
+            <NavLink
+              to={"products"}
+              className="underline text-sm text-green-500"
+            >
+              مشاهده همه محصولات
+            </NavLink>
+          </div>
           <LastProducts />
         </div>
         <div className="bg-white p-5 rounded-md">
-          <h3 className="font-MorabbaMedium mb-5">آخرین کاربران اضافه شده</h3>
+          <div className="flex justify-between">
+            <h3 className="font-MorabbaMedium mb-5">آخرین کاربران اضافه شده</h3>
+            <NavLink to={"users"} className="underline text-sm text-green-500">
+              مشاهده همه کاربران
+            </NavLink>
+          </div>
           <LastUsers />
         </div>
       </div>
--- src/Pages/Home/components/Card.jsx
+++ src/Pages/Home/components/Card.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { FaBox, FaMoneyBillAlt, FaUsers } from "react-icons/fa";
 import { RiArrowUpLine } from "react-icons/ri";
 
 function Card({ title, total, totalTitle, increase, lastSevenDays }) {
   return (
     <div className="bg-white w-full rounded-md p-5 shadow-lg shadow-gray-200">
-      <div className="">
-        <span className="font-MorabbaMedium block">{title}</span>
-        <span className="text-gray-400 text-sm">7 روز گذشته</span>
+      <div className="flex justify-between items-center border-b border-gray-200 pb-5">
+        <div>
+          <span className="font-MorabbaMedium block">{title}</span>
+          <span className="text-gray-400 text-sm">7 روز گذشته</span>
+        </div>
+
+        <div>
+          {title === "همه سفارشات" && (
+            <div className="bg-indigo-500 text-white p-5 rounded-full">
+              <FaBox size={20} />
+            </div>
+          )}
+          {title === "همه کاربران" && (
+            <div className="bg-lime-500 text-white p-5 rounded-full">
+              <FaUsers size={20} />
+            </div>
+          )}
+          {title === "همه فروش" && (
+            <div className="bg-pink-500 text-white p-5 rounded-full">
+              <FaMoneyBillAlt size={20} />
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="space-x-2 flex items-center mt-2">
@@ -19,17 +40,6 @@ function Card({ title, total, totalTitle, increase, lastSevenDays }) {
           <RiArrowUpLine />
         </span>
       </div>
-
-      <div className="flex gap-2 text-sm mt-2">
-        <span className="text-gray-400 text-sm">7 روز گذشته</span>
-        <span className="text-blue-700 font-DanaDemiBold">
-          (${lastSevenDays})
-        </span>
-      </div>
-
-      <button className="border border-blue-500 text-blue-500 text-sm py-1 px-2 mt-5 rounded-full w-20">
-        جزییات
-      </button>
     </div>
   );
 }
